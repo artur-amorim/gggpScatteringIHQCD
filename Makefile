0445b3dca15e2e0d8294b74b20042ed620055c3f
Artur Amorim <arturj.c.amorimdesousa@gmail.com>
2020-03-16 13:42:54 +0800

    Added PhotonScattering class.

--- Makefile
+++ Makefile
@@ -12,7 +12,10 @@ clean:
 
 # test code
 
-tests: bin/test_U1NNMode.exe bin/test_schrodinger.exe bin/test_Kernel.exe bin/test_alphaQED.exe
+tests: bin/test_U1NNMode.exe bin/test_schrodinger.exe bin/test_Kernel.exe bin/test_alphaQED.exe bin/test_PhotonScattering.exe
+
+bin/test_PhotonScattering.exe: bin/tests/test_PhotonScattering.o bin/PhotonScattering.o bin/Reggeon.o bin/Spectra.o
+	$(CXX) -o $@ $^ $(LIBS)
 
 bin/test_alphaQED.exe: bin/tests/test_alphaQED.o bin/alphaQED.o
 	$(CXX) -o $@ $^ $(LIBS)
@@ -21,7 +24,7 @@ bin/test_Kernel.exe: bin/tests/test_Kernel.o bin/IHQCD.o bin/Reggeon.o bin/Kerne
 	$(CXX) -o $@ $^ $(LIBS)
 
 bin/test_U1NNMode.exe: bin/tests/test_U1NNMode.o bin/U1NNMode.o bin/IHQCD.o bin/Fortran/colnew.o bin/Fortran/dgefa.o bin/Fortran/dgesl.o
-	$(CXX) -o $@ $^ -lblas $(LIBS_GFORTRAN)
+	$(CXX) -o $@ $^ $(LIBS) $(LIBS_GFORTRAN)
 
 bin/test_schrodinger.exe: bin/tests/test_schrodinger.o bin/schrodinger/common.o bin/schrodinger/solvspec.o bin/schrodinger/chebspec.o bin/schrodinger/numerov.o bin/schrodinger/schrodinger.o
 	$(CXX) -o $@ $^ $(LIBS)
--- include/PhotonScattering.h
+++ include/PhotonScattering.h
@@ -1,5 +1,5 @@
-#ifndef PROCOBS_H
-#define PROCOBS_H
+#ifndef GAMMASCAT_H
+#define GAMMASCAT_H
 
 #include <vector>
 #include "Spectra.h"
@@ -14,36 +14,33 @@ struct kinStruct
     bool operator>(const kinStruct &rhs) const;
 };
 
-class ProcessObservable
+class PhotonScattering
 {
     private :
-        bool rsslog ; // True if we fit log of the observable. False otherwise
         std::vector< std::vector<double> > datapts ;
     protected:
-        void copy(const ProcessObservable & rhs);
+        void copy(const PhotonScattering & rhs);
         virtual void  loadData(std::string file_path = "");
     public :
-        ProcessObservable(const bool rrsslog = false);
-        ProcessObservable(const ProcessObservable &proc);
-        bool getRSSLOG();
+        PhotonScattering();
+        PhotonScattering(const PhotonScattering &proc);
         std::vector< std::vector<double> > getDataPts();
-        void setRSSLOG(const bool rrsslog);
         void setDataPts(const std::vector< std::vector<double> > &pts );
         virtual std::vector<double> expVal();
         virtual std::vector<double>  expErr();
         virtual std::vector<std::vector<double> >  expKinematics();
-        virtual std::vector<double>  getNeededTVals();
+        std::vector<double>  getNeededTVals();
         virtual double IzN(const std::vector<double> &kin, const Reggeon &reg);
         virtual double IzNBar(const std::vector<double> &kin, const Reggeon &reg, const std::vector<double> &gs);
         virtual std::vector<kinStruct>  getIzs(const std::vector< std::vector<double> > &points, const std::vector<Spectra> &spec);
         virtual std::vector<kinStruct>  getIzsBar(const std::vector< std::vector<double> >  &points,
                                                   const std::vector<Spectra> &spec,
                                                   const std::vector<double>  &gs);
-        virtual std::vector<double>  predictObs(const std::vector<kinStruct>  &Izs, const std::vector<kinStruct>  &IzsBar,
-                                                const std::vector< std::vector<double> >  &points, const bool savePredictions);
+        virtual std::vector<double>  predict(const std::vector<kinStruct>  &Izs, const std::vector<kinStruct>  &IzsBar,
+                                                const std::vector< std::vector<double> >  &points, const bool savePredictions = false);
         std::vector<double>  diffObsWeighted(const std::vector<kinStruct>  &Izs, const std::vector<kinStruct>  &IzsBar, const std::vector< std::vector<double> >  &points);
         double rss(const std::vector<kinStruct>  &Izs, const std::vector<kinStruct>  &IzsBar, const std::vector< std::vector<double> >  &points) ;   
-        ProcessObservable& operator= (const ProcessObservable &procs);
-        virtual ~ProcessObservable();
+        PhotonScattering& operator= (const PhotonScattering &procs);
+        virtual ~PhotonScattering();
 };
 #endif
