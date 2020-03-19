398b8c75ad4814cc18ac4711150cfefaa1f19b5c
Artur Amorim <arturj.c.amorimdesousa@gmail.com>
2020-03-19 13:03:44 +0800

    Adapted HQCDP for this project.

--- include/HQCDP.h
+++ include/HQCDP.h
@@ -2,7 +2,7 @@
 #define HQCDP_H
 
 #include <vector>
-#include "ProcessObservable.h"
+#include "PhotonScattering.h"
 #include "Kernel.h"
 #include "Spectra.h"
 
@@ -11,23 +11,21 @@ struct NelderMead;
 class HQCDP
 {
 private:
-    const bool rsslog ;                                                    // fit the log of the processes' observables: true or false
-    const double SPconstraint;                                             // Weight in order to enforce the soft pomeron trajectory intercept
-    std::vector<ProcessObservable *> processes;                            // Processes relevant for the computation
-    std::vector<double> useTVals ;                                         // Values of t
-    std::vector<Kernel *> kernels;                                         // Kernels that are going to be used
-    std::vector<Spectra> spectrum;                                         // Object that contains the relevant reggeons
-    std::vector<double> gns;                                               // Couplings parameters
-    double operator()(const std::vector<double> &x);                       // Auxiliary function to fit data
-    friend NelderMead;                                                     // Allow NelderMead struct to have access to operator()                                                           
+    std::vector<PhotonScattering *> processes;                              // Processes relevant for the computation
+    std::vector<double> useTVals ;                                          // Values of t
+    std::vector<Kernel *> kernels;                                          // Kernels that are going to be used
+    std::vector<Spectra> spectrum;                                          // Object that contains the relevant reggeons
+    std::vector<double> gns;                                                // Couplings parameters
+    double operator()(const std::vector<double> &x);                        // Auxiliary function to fit data
+    friend NelderMead;                                                      // Allow NelderMead struct to have access to operator()                                                           
 public:
-    HQCDP(const bool rrsslog = false, const double spconst = 1e2);                                  // HQCDP constructor                                                                                     // HQCDP destructor
+    HQCDP();                                                                                        // HQCDP constructor
     void setGNs(const std::vector<double>& gs);                                                     // Set gns
     std::vector<Kernel *> getKernels() const;                                                       // Get Kernels
     std::vector<Spectra> getSpectrum() const;                                                       // Get Spectrum
     std::vector<double> getUseTVals() const;                                                        // Get the tvals to be used
     void computeNeededTVals();                                                                      // Get all the ts to fit or predict the processes
-    void addProcessObservable(ProcessObservable &proc);                                             // Add a process to be fitted or predicted
+    void addProcessObservable(PhotonScattering &photon);                                            // Add a process to be fitted or predicted
     void addKernel(Kernel &f);                                                                      // Add a kernel to be used in the fit or prediction
     void computeSpectrum(const std::vector< std::vector<double> > &kernelPars = {});                // Vector wish contains vectors with the parameters of each kernel
     int NumberOfDegreesOfFreedom();                                                                 // Computes the number of degrees of freedom of a fit
--- src/HQCDP.cpp
+++ src/HQCDP.cpp
@@ -3,8 +3,7 @@
 #include "HQCDP.h"
 #include "methods/optimization/NelderMead.hpp"
 
-HQCDP::HQCDP(const bool rrsslog, const double spconst):
-            rsslog(rrsslog), SPconstraint(spconst),
+HQCDP::HQCDP():
             processes({}), useTVals({}),
             kernels({}), spectrum({}),
             gns({})
@@ -39,15 +38,9 @@ void HQCDP::computeNeededTVals()
     for(int i = 0; i < nprocs ; i++)
     {
         std::vector<double> proc_ts = processes[i]->getNeededTVals();
-        if(proc_ts.size() == 0)
-        {
-            std::cout << "Process with no t values has been found" << std::endl;
-        }
+        if(proc_ts.size() == 0) std::cout << "Process with no t values has been found" << std::endl;
         // Get all the tvals in proc_ts
-        for(int j = 0 ; j < proc_ts.size(); j++)
-        {
-            useTVals.push_back(proc_ts[j]) ;
-        }
+        for(int j = 0 ; j < proc_ts.size(); j++) useTVals.push_back(proc_ts[j]) ;
     }
     // Now we std::sort the values of t and remove repeated values
     std::sort(useTVals.begin(), useTVals.end()) ;
@@ -55,13 +48,12 @@ void HQCDP::computeNeededTVals()
     useTVals.erase(std::unique(useTVals.begin(), useTVals.end() ), useTVals.end() );
 }
 
-void HQCDP::addProcessObservable(ProcessObservable &proc)
+void HQCDP::addProcessObservable(PhotonScattering &photon)
 {
     // Add a process to be fitted or predicted
     // For consistency change NMC, alpha and rsslog data members of proc
     // to be the same as the HQCDP object.
-    ProcessObservable * newProcess = &proc;
-    if(proc.getRSSLOG() != rsslog) newProcess->setRSSLOG(rsslog);
+    PhotonScattering * newProcess = &photon;
     // Append the process observable proc
     processes.push_back(newProcess) ;
 }
@@ -91,10 +83,7 @@ void HQCDP::computeSpectrum(const std::vector< std::vector<double> > &kernelPars
        }
    }
    // Compute all the needed tvals if not computed
-   if(useTVals.size() == 0)
-   {
-       computeNeededTVals();
-   }
+   if(useTVals.size() == 0) computeNeededTVals();
    // for each value of t we are going to compute the Reggeons of each Kernel object in kernels
    // Clean previous spectrum
    spectrum.clear();
@@ -126,14 +115,10 @@ int HQCDP::NumberOfDegreesOfFreedom()
         the number of data points. Then we iterate over the kernels and compute the number
         of kernel parameters. Finally we add to the number of kernel parameters the number of gns.
     */
-   // Iterate over the processes vector
+   // Iterate over the processes vector to count the number of experimental points
    int nPoints = 0;
    for(int i = 0; i < processes.size(); i++) nPoints += processes[i]->getDataPts()[0].size();
-   // Iterate over the kernel vector;
-   int nfitPars = 0;
-   for(int i = 0; i < kernels.size(); i++) nfitPars += kernels[i]->KernelPars().size();
-   // Add to nfitPars the number of gns
-   nfitPars += gns.size();
+   int nfitPars = gns.size();
    // # of degrees of freedom = Number of fitting Points - Number of fitting parameters
    return nPoints - nfitPars;
 }
@@ -149,7 +134,7 @@ double HQCDP::chi2()
     double chi2 = 0.0;
     for(int i = 0; i < processes.size(); i++)
     {
-        ProcessObservable * proc = processes[i];
+        PhotonScattering * proc = processes[i];
         std::vector<std::vector<double> > points = proc->expKinematics();
         std::vector<kinStruct> izs = proc->getIzs(points, spectrum);
         std::vector<kinStruct> izsbar = proc->getIzsBar(points, spectrum, gns);
@@ -162,27 +147,15 @@ double HQCDP::operator()(const std::vector<double> &x)
 {
     /* 
         The operator() is used as the fitting function.
-        It uses the values of x to update the kernel parameters
-        as well the gn parameters.
-        Then it updates the spectrum since we have new kernel parameters.
-        Afther that we also update the GNs values.
+        It uses the values of x to update the gn parameters.
         After that we compute the new chi2 value and it is returned
     */
-    // Kernel pars correspond to x(0), x(1), x(2), x(3), x(4)
-    std::vector<double> gluon_pars = {x[0]};
-    std::vector<std::vector<double> > kernels_pars = {gluon_pars};
-    // GNs correspond to x(5), ..., x(n_Kernel_regs+4)
-    const int n_Kernel_regs = kernels[0]->NTrajectories();
-    std::vector<double> gpars(n_Kernel_regs, 0);
-    for(int i = 0; i < n_Kernel_regs; i++) gpars[i] = x[5+i];
-    // Update the spectrum
-    computeSpectrum(kernels_pars);
     // Update the GNs
-    setGNs(gpars);
+    setGNs(x);
     // Compute chi2
     double CHI2 = chi2();
-    std::cout << "invls: " << x[0] << " a: " << x[1] << " b: " << x[2] << " c: " << x[3] << " d: " << x[4];
-    std::cout << "g1: " << x[5] << " g2: " << x[6] << " g3: " << x[7] <<  " g4: " << x[8] << std::endl;
+    for(int i = 0; i < x.size(); i++) std::cout << "g" + std::to_string(i+1) << ": " << x[i] << " ";
+    std::cout << std::endl;
     std::cout << "chi2: " << CHI2 << std::endl;
     // Return chi2
     return CHI2;
@@ -203,35 +176,15 @@ void HQCDP::fit(const std::vector<double> &xguess, const double delta)
     int ndof  = this->NumberOfDegreesOfFreedom();
     // Optimize using Nelder-Mead algorithm
     std::function<double(std::vector<double>)> f = [this] (const std::vector<double> x) {return this->operator()(x);};
-    Fit bestFit = optimFunction(xguess, f, delta);
-    std::vector<double> xopt = bestFit.bestFitPars;
-    std::vector<double> xoptUncert = bestFit.bestFitParsUncert;
-    // Compute chi2
-    // Kernel pars correspond to xopt(0), xopt(1), xopt(2), xopt(3), xopt(4)
-    std::vector<double> gluon_pars = {xopt[0], xopt[1], xopt[2], xopt[3], xopt[4]};
-    std::vector<std::vector<double> > kernels_pars = {gluon_pars};
-    // GNs correspond to x(5), ..., x(n_Kernel_regs+4)
-    const int n_Kernel_regs = kernels[0]->NTrajectories();
-    std::vector<double> gpars(n_Kernel_regs, 0);
-    for(int i = 0; i < n_Kernel_regs; i++) gpars[i] = xopt[5+i];
-    // NMC coupling alpha correspond to x(n_Kernel_regs+5)
-    double beta;
-    // gtildes correspon to x(n_Kernel_regs+6), ... x(n_Kernel_regs+6+n_Kernel+regs - 1)    
-    // Update the spectrum
-    computeSpectrum(kernels_pars);
+    std::vector<double> xopt = optimFunction(xguess, f, delta);
     // Update the GNs
-    setGNs(gpars);
+    setGNs(xopt);
     // Compute chi2
     double CHI2 = chi2();
    // Print the results
    std::cout << "Best chi2 found for:" << std::endl;
-   std::cout << "invls:\t" << gluon_pars[0] << "+-" << xoptUncert[0] << std::endl;
-   std::cout << "a:\t"     << gluon_pars[1] << "+-" << xoptUncert[1] << std::endl;
-   std::cout << "b:\t"     << gluon_pars[2] << "+-" << xoptUncert[2] << std::endl;
-   std::cout << "c:\t"     << gluon_pars[3] << "+-" << xoptUncert[3] << std::endl;
-   std::cout << "d:\t"     << gluon_pars[4] << "+-" << xoptUncert[4] << std::endl;
    // Print the GNs
-   for(int i = 0; i < gpars.size(); i++) std::cout << "g" << std::to_string(i+1) << '\t' << gpars[i] << " +- " << xoptUncert[5+i] << std::endl;
+   for(int i = 0; i < xopt.size(); i++) std::cout << "g" << std::to_string(i+1) << '\t' << xopt[i] << std::endl;
    std::cout << "chi2:\t"  << CHI2 << std::endl;
    std::cout << "Number of degrees of freedom (Ndof):\t" << ndof << std::endl;
    std::cout << "chi2/Ndof:\t" << CHI2/ndof << std::endl;
